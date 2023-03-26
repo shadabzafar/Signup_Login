f0fa5e2c30f226f1b8eb4ca8cb6f8370ecc76117
shadabzafar <shadabzafar2838@gmail.com>
2023-03-26 22:17:44 +0530

    Get data from parse server

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -2,22 +2,32 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
+
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
 
-    private Button btnSave;
+    private Button btnSave, btnGetAllData;
     private EditText edtName, edtPunchSpeed, edtPunchPower, edtKickSpeed, edtKickPower;
+    private TextView txtGetData;
+    private String allKickBoxers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +41,59 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         edtKickSpeed = findViewById(R.id.edtKickSpeed);
         edtKickPower = findViewById(R.id.edtKickPower);
         btnSave = findViewById(R.id.btnSave);
+        txtGetData = findViewById(R.id.txtGetData);
+        btnGetAllData = findViewById(R.id.btnGetAllData);
 
         btnSave.setOnClickListener(SignUp.this);
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("qBkyNvFvqY", new GetCallback<ParseObject>() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object != null && e == null){
+                            txtGetData.setText("name: " + object.get("name") + "\n" + "punch speed: " + object.get("punch_speed")
+                            + "\n" + "punch power: " + object.get("punch_power") + "\n" + "kick speed: " + object.get("kick_speed")
+                            + "\n" + "kick power: " + object.get("kick_power"));
+                        }
+                    }
+                });
+
+            }
+        });
+
+        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                allKickBoxers = "";
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("KickBoxer");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e == null){
+                            if(objects.size() > 0){
+                                for(ParseObject kickBoxer: objects) {
+                                  allKickBoxers = allKickBoxers + "name: " + kickBoxer.get("name") + "\n" + "punch speed: " + kickBoxer.get("punch_speed")
+                                          + "\n" + "punch power: " + kickBoxer.get("punch_power") + "\n" + "kick speed: " + kickBoxer.get("kick_speed")
+                                          + "\n" + "kick power: " + kickBoxer.get("kick_power") + "\n\n";
+                                }
+                                txtGetData.setText(allKickBoxers);
+
+                                FancyToast.makeText(SignUp.this, "Success", FancyToast.LENGTH_LONG,
+                                        FancyToast.SUCCESS, true).show();
+                            }
+                            else {
+                                FancyToast.makeText(SignUp.this, e.getMessage(),FancyToast.LENGTH_LONG,
+                                        FancyToast.ERROR,true).show();
+                            }
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
